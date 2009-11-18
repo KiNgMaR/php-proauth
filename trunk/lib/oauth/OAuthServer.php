2715d1593594cc4a62bbb7d99eff0a3dd2c335ee
ingmar.runge <ingmar.runge@090a6256-d42f-11de-8184-f791cb96e2ed>
2009-11-18 23:22:26 +0000

    Moving abstract class OAuthSignatureMethod where it belongs, a separate file.

--- trunk/lib/oauth/OAuthServer.php
+++ trunk/lib/oauth/OAuthServer.php
@@ -251,39 +251,3 @@ class OAuthServerRequest extends OAuthRequest
 	}
 }
 
-
-abstract class OAuthSignatureMethod
-{
-	/**
-	 * Must return the name of the method, e.g. HMAC-SHA1 or PLAINTEXT.
-	 * @return string
-	 **/
-	abstract public function getName();
-	/**
-	 * Must build the signature string from the given parameters and return it.
-	 * @return string
-	 **/
-	abstract protected function buildSignature(OAuthRequest $req, OAuthConsumer $consumer, OAuthToken $token);
-
-	/**
-	 * Compares the given $signature_string with the one that is defined by req, consumer and token.
-	 * If $signature_string is NULL, the oauth_signature parameter from $req will be used.
-	 * @return bool
-	 **/
-	public function checkSignature(OAuthRequest $req, OAuthConsumer $consumer, OAuthToken $token, $signature_string = NULL)
-	{
-		$correct_string = $this->buildSignature($req, $consumer, $token);
-
-		if(is_null($signature_string))
-		{
-			$signature_string = $req->getSignatureParameter();
-		}
-
-		// extra checks to make sure we never allow obviously faulty signature strings:
-		return (is_string($signature_string) &&
-			is_string($correct_string) &&
-			!empty($signature_string) &&
-			strcmp($correct_string, $signature_string) == 0);
-	}
-}
-
