ab18ef3a6d274e9c77ed0f177ced90dd2d44e0a8
ingmar.runge <ingmar.runge@090a6256-d42f-11de-8184-f791cb96e2ed>
2009-11-20 10:50:31 +0000

    Adding Service Provider-side support for oauth_problem as defined here: http://oauth.pbworks.com/ProblemReporting

--- trunk/lib/oauth/OAuthServerBackend.php
+++ trunk/lib/oauth/OAuthServerBackend.php
@@ -6,6 +6,10 @@ abstract class OAuthServerBackend
 	public const RESULT_OK = 1;
 	public const RESULT_DUPE = 2;
 
+	public const RESULT_RATE_LIMITED = 3;
+	public const RESULT_NOT_FOUND = 4;
+	public const RESULT_DISABLED = 5;
+
 	abstract public function getConsumerByKey($consumer_key);
 	abstract public function checkNonceAndTimeStamp($nonce, $timestamp, OAuthConsumer $consumer, OAuthToken $token);
 	abstract public function addTempToken(OAuthConsumer $consumer, OAuthToken $new_token, $callback_url);
