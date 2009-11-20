5268bf8e45d845d78733a2c32ea9cbb402466ae7
ingmar.runge <ingmar.runge@090a6256-d42f-11de-8184-f791cb96e2ed>
2009-11-20 20:21:41 +0000

    Fixing some paths...

--- trunk/tests/oauth/OAuthRequestTest.php
+++ trunk/tests/oauth/OAuthRequestTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../lib/oauth/OAuthRequest.php';
+require_once '../../lib/oauth/OAuthClient.php';
 require_once 'PHPUnit/Framework.php';
 
 class OAuthRequestTest extends PHPUnit_Framework_TestCase
