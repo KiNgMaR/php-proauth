f29d97f578ed2558417bce9391aa887ac8e0dcd5
ingmar.runge <ingmar.runge@090a6256-d42f-11de-8184-f791cb96e2ed>
2009-12-15 02:17:03 +0000

    Some very basic server documentation and some fixes...

--- trunk/lib/oauth/OAuthServerBackend.php
+++ trunk/lib/oauth/OAuthServerBackend.php
@@ -14,15 +14,56 @@ abstract class OAuthServerBackend
 	public const RESULT_BAD_TIMESTAMP = 7;
 	public const RESULT_BAD_TOKEN = 8;
 
+	/**
+	 * @param string consumer_key
+	 * @return mixed Return an OAuthConsumer instance, or one of: RESULT_RATE_LIMITED, RESULT_DISABLED, RESULT_NOT_FOUND
+	 **/
 	abstract public function getConsumerByKey($consumer_key);
+
+	/**
+	 * @return int One of: RESULT_OK, RESULT_DUPE_NONCE, RESULT_BAD_TIMESTAMP, RESULT_BAD_TOKEN
+	 **/
 	abstract public function checkNonceAndTimeStamp($nonce, $timestamp, OAuthConsumer $consumer, OAuthToken $token = NULL);
+
+	/**
+	 * Creates a new temporary token/key pair, associated with $consumer and optionally $callback_url.
+	 * @return int One of: RESULT_DUPE (the token string is already used), RESULT_OK
+	 **/
 	abstract public function addTempToken(OAuthConsumer $consumer, OAuthToken $new_token, $callback_url);
+
+	/**
+	 * @return int Return RESULT_OK or RESULT_ERROR.
+	 **/
 	abstract public function checkTempToken(OAuthConsumer $consumer, $token_str, $callback_url, $user_idf, $authed_status, &$token_secret);
+
+	/**
+	 * @return int Return RESULT_OK or RESULT_ERROR.
+	 **/
 	abstract public function authorizeTempToken($token_str, $user_idf, $verifier);
+
+	/**
+	 * @return int Return RESULT_OK or RESULT_ERROR.
+	 **/
 	abstract public function deleteTempToken($token_str, $user_idf);
+
+	/**
+	 * @return string
+	 **/
 	abstract public function getTempTokenCallback(token_str, $user_idf);
+
+	/**
+	 * @return string
+	 **/
 	abstract public function generateVerifier($callback_url);
+
+	/**
+	 * @return int One of: RESULT_DUPE (the token string is already used), RESULT_OK
+	 **/
 	abstract public function exchangeTempToken(OAuthConsumer $consumer, OAuthToken $temp_token, OAuthToken $new_token);
+
+	/**
+	 * @return int Return RESULT_OK or RESULT_ERROR.
+	 **/
 	abstract public function getAccessTokenInfo(OAuthConsumer $consumer, $token_str, &$token_secret, &$user_data);
 }
 
