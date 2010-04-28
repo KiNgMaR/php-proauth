ab54bf20b1ef8030b67ae09da86ebe90dc5a84a6
ingmar.runge <ingmar.runge@090a6256-d42f-11de-8184-f791cb96e2ed>
2010-04-28 10:01:57 +0000

    Working on the web_server flow.

--- trunk/lib/oauth/OAuth2Util.php
+++ trunk/lib/oauth/OAuth2Util.php
@@ -21,6 +21,18 @@ class OAuth2Exception extends Exception
 
 class OAuth2AccessToken
 {
+	protected $access_token;
+	protected $expires_at;
+	protected $refresh_token;
+	protected $token_secret;
 
+	public function __construct($access_token, $expires_at = 0,
+		$refresh_token = '', $access_token_secret = '')
+	{
+		$this->access_token = $access_token;
+		$this->expires_at = (int)$expires_at;
+		$this->refresh_token = $refresh_token;
+		$this->token_secret = $access_token_secret;
+	}
 }
 
